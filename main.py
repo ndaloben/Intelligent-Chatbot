4117bce5ca94692ac148d6224aedfffaffa8fae9
ben <ndaloben8@gmail.com>
2023-09-13 19:00:24 +0300

    best match

--- main.py
+++ main.py
@@ -11,3 +11,26 @@ def save_knowledge_base(file_path: str, data: dict):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
  
+ 
+def find_best_match(user_question: str, questions: list[str]) -> str | None:
+     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+     return matches[0] if matches else None
+ 
+def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
+    for q in knowledge_base["questions"]:
+        if q["question"] == question:
+            return q["answer"]
+        
+def chat_bot():
+    knowledge_base: dict = load_Knowledge_base('knowledge_base.json')
+    
+    
+    while True: 
+        user_input: str = input('You:')
+        
+        if user_input.lower() == 'quit':
+            break
+        
+        best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+    
+            
